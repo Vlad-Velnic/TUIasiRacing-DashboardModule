c5805d7a46324ccff3fdf26dd838fd25111c101f
Vlad-Velnic <vlad.velnic@gmail.com>
2025-04-07 17:55:28 +0300

    implemented SD writer initialization and logfile creation

--- Dashboard/src/dashboard.h
+++ Dashboard/src/dashboard.h
@@ -6,34 +6,34 @@
 #include <Wire.h>
 #include "RTClib.h"
 #include <Adafruit_NeoPixel.h>
-
-
+#include <SD.h>
+#include <SPI.h>
 
 // Numeric constants
 
-#define NUM_LEDS    8        // Number of LEDs in the RPM strip
-#define MAX_RPM     8000     // Max engine RPM
-#define MIN_RPM     4000     // Min engine RPM
-
-
+#define NUM_LEDS 8   // Number of LEDs in the RPM strip
+#define MAX_RPM 8000 // Max engine RPM
+#define MIN_RPM 4000 // Min engine RPM
 
 // Pins
 
-#define RPM_PIN     5        // Data pin to LED strip for RPM
-
-
+#define RPM_PIN 5      // Data pin to LED strip for RPM
+#define SD_CS_PIN 15   // Chip select pin for SD writer
+#define SD_SCK_PIN 18  // SCK pin for SD writer
+#define SD_MISO_PIN 19 // MISO pin for SD writer
+#define SD_MOSI_PIN 23 // MOSI pin for SD writer
 
 // Global variables and objects
 
 extern RTC_DS1307 rtc;
-extern DateTime timestamp; //initializat cu o valoare absurda
+extern DateTime timestamp;
 extern Adafruit_NeoPixel trmetru;
 extern int currentRPM;
-
-
+extern String filename;
 
 // Methods
 
 void printTime();
 void printDateAndTime();
 void showRPM(int currentRPM);
+String pad(int number);
--- Dashboard/src/main.cpp
+++ Dashboard/src/main.cpp
@@ -1,36 +1,66 @@
 #include "dashboard.h"
 
 RTC_DS1307 rtc;
-DateTime timestamp(1999, 1, 1, 0, 0, 0); // initializat cu o valoare absurda
+DateTime timestamp(1999, 1, 1, 0, 0, 0); // initialized with an absurd value
 Adafruit_NeoPixel trmetru(NUM_LEDS, RPM_PIN, NEO_GRB + NEO_KHZ800);
 int currentRPM = 0;
+String filename = "/dump";
 
-
-void setup() {
+void setup()
+{
   Serial.begin(115200);
   Wire.begin();
 
-  // Initializare RTC
-  while (!rtc.begin()) {
+  // Initialize RTC
+  while (!rtc.begin())
+  {
     Serial.println("Nu s-a putut initializa RTC-ul!");
-    sleep(100);
+    sleep(10);
   }
+  timestamp = rtc.now();
 
-  // Initializare TRMETRU
+  // Initialize TRMETRU
   trmetru.begin();
   trmetru.show();
 
+  // Initialize SD writer
+  SPI.begin(SD_SCK_PIN, SD_MISO_PIN, SD_MOSI_PIN, SD_CS_PIN);
+  while (!SD.begin(SD_CS_PIN))
+  {
+    Serial.println("SD card initialization failed!");
+    sleep(10);
+  }
 
-  // 
-  timestamp = rtc.now();
-  Serial.print("/nInitialization done at: "); printTime();
+  // Create logfile
+  filename = "/log_" +
+             String(timestamp.year()) + "-" +
+             pad(timestamp.month()) + "-" +
+             pad(timestamp.day()) + "_" +
+             pad(timestamp.hour()) + "-" +
+             pad(timestamp.minute()) + "-" +
+             pad(timestamp.second()) + ".txt";
+
+  File dataFile = SD.open(filename, FILE_WRITE);
+  if (dataFile)
+  {
+    Serial.print("Logging data to: ");
+    Serial.println(filename);
+    dataFile.close();
+  }
+  else
+  {
+    Serial.println("Error opening file.");
+  }
+
+  // SETUP DONE
+  Serial.print("/nInitialization done at: ");
+  printTime();
   Serial.println("/n********LOOP Start********/n/n");
 }
 
-void loop() {
+void loop()
+{
   timestamp = rtc.now();
   currentRPM = (millis() / 10) % MAX_RPM; // just cycles up and down for demo
-
   showRPM(currentRPM);
 }
-
